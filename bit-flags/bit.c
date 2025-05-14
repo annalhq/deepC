874aaece1672236b42d709234607159eb301cf38
annalhq <annalhqshaikh@gmail.com>
2025-05-14 20:42:36 +0530

    chore(add): bit.c

---
+++ bit-flags/bit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef unsigned int t_flag;
+#define FLAG_A (1 << 0)
+#define FLAG_B (1 << 1)
+#define FLAG_C (1 << 2)
+
+
+int f(int x, t_flag flags){
+  if (flags & FLAG_A) x += x;
+  if (flags & FLAG_B) x *= x;
+  if (flags & FLAG_C) x = ~x;
+  return x;
+}
+
+int main(int argc, char *argv[])
+{
+  printf("%d\n", f(1234, 0));
+  printf("%d\n", f(1234, FLAG_A));
+  printf("%d\n", f(1234, FLAG_B | FLAG_C));
+
+  return EXIT_SUCCESS;
+}
